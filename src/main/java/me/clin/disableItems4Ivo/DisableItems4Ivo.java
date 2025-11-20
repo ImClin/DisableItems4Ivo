bfb54d3b0c3bfe60e536657d6f1ea470c5aa930d
Colin Canters <colincanters@gmail.com>
2025-11-20 18:15:56 +0100

    Disable items with permissions

---
+++ src/main/java/me/clin/disableItems4Ivo/DisableItems4Ivo.java
@@ -0,0 +1,17 @@
+package me.clin.disableItems4Ivo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisableItems4Ivo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
